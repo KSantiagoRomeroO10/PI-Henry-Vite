ec71deb86782f8b7a6b429a3e4a208d24816dcb0
Santiago Romero <KevinSRomeroO10@hotmail.com>
2023-11-14 16:12:44 -0500

    Agregando un while que trabaja con el parametro page de la página.

--- server/Source/Controllers/GetVideogamesController.js
+++ server/Source/Controllers/GetVideogamesController.js
@@ -5,18 +5,32 @@ const Videogame = require('../Models/Videogame')
 
 const GetVideogamesController = async(req, res) => {
   try{
-    const dbVideogamesResponse = await Videogame.findAll()
+    const dbVideogamesResponse = await Videogame.findAll()    
 
-    const apiResponse = await axios.get(`${process.env.API_URL}?key=${process.env.API_KEY}`)
+    let pages = true
+    let numberPage = 1
+    const apiResponseAllPage = []
 
-    const apiData = apiResponse.data.results
+    while(pages) {
 
-    const newFormat = apiData.map(videogame => {
+      let apiResponse = await axios.get(`${process.env.API_URL}?key=${process.env.API_KEY}&page=${numberPage}`)
+      apiResponseAllPage.push(...apiResponse.data.results)
+      numberPage += 1 
+
+      if(numberPage === 6) pages = false
+      // if(apiResponse.data.next === null) pages = false
+      // else console.log(`Página: ${numberPage}`);
+
+    }
+
+    // const apiData = apiResponse.data.results
+
+    const newFormat = apiResponseAllPage.map(videogame => {
       const platforms = videogame.platforms.map(platform => platform.platform)
       return {
         id: videogame.id,
         nombre: videogame.name,
-        descripcion: apiData.description_raw && apiData.description,
+        descripcion: videogame.description,
         plataformas: platforms,
         imagen: videogame.background_image,
         fechaLanzamiento: videogame.fechaLanzamiento,
